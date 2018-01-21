34e061a0e7a264107df482560fc52b364b62f222
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-01-21 16:41:07 +0530

    Create LEAP.C
    find year is leap or not

---
+++ LEAP.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+clrscr();
+printf("enter the year");
+scanf("%d",&year);
+if(year=2016)
+{
+printf(:"the year is leap");
+}
+else
+{
+printf(" the year is not leap");
+}
+getch();
+}
