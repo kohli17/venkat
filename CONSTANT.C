4537ef121fc14a445373bcb36b08f7783f267d69
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-01-21 16:11:23 +0530

    Create CONSTANT.C
    the value is vowel or constant

---
+++ CONSTANT.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("the value of a,b");
+scanf("%d%d"'&a,&b);
+if (a=vowel)
+{
+printf(''the value is vowel");
+}
+else(b=constsny)
+{
+printf("thr value is condtant");
+}
+getch();
+}
