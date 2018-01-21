1cfcdead69f11c864a117c7b46f7b1a58ea10893
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-01-21 16:30:46 +0530

    Create LARGE.C
    find largest number

---
+++ LARGE.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("enter the value of a,b,c");
+scanf("%d%d%d",&a,&b,%c);
+if(a>b)
+{
+printf("the value of a");
+}
+elseif(b>c)
+{
+printf(the value of b");
+}
+
+elseif(c>a)
+{
+printf("the valuwes of c");
+}
+grtch();
+}
+else
+{
+printf(the values og largest number");
+}
+getch();
+}
